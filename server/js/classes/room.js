1e89e44b5a73e3963f99808bffbddc8c34555e56
Reuben Bradley <reuben.m.bradley@gmail.com>
2017-01-22 02:45:04 +0000

    Rooms now generate a viable path for hazard generation

--- server/js/classes/room.js
+++ server/js/classes/room.js
@@ -4,6 +4,7 @@
 'use strict';
 
 var Util = require('./util');
+var Path = require('./path');
 
 var Room = function( roomId, roomsize, bounds ) {
     this.roomId = roomId;
@@ -15,23 +16,22 @@ var Room = function( roomId, roomsize, bounds ) {
     this.roomsize = roomsize;
     this.tiles = [];
     
-    var isEdgeTile = function(x, y) {
-        if ( x === 0 || x === roomsize - 1 ) {
-            return true;
-        }
-        else if ( y === 0 || y === roomsize - 1 ) {
-            return true;
-        }
-        return false;
-    };
-    
     // Initialise the tiles array
     for ( var x = 0; x < roomsize; x++ ) {
         this.tiles[x] = [];
         for ( var y = 0; y < roomsize; y++ ) {
-            this.tiles[x].push( ( isEdgeTile(x, y) ) ? 1 : 0 );
+            this.tiles[x].push( ( this.isEdgeTile(x, y) ) ? 1 : 0 );
         }
     }
+};
+Room.prototype.isEdgeTile = function( x, y ) {
+    if ( x === 0 || x === this.roomsize - 1 ) {
+        return true;
+    }
+    else if ( y === 0 || y === this.roomsize - 1 ) {
+        return true;
+    }
+    return false;
 };
 Room.prototype.connectToRoom = function( room ) {
     var direction = this.determineDirectionOf(room);
@@ -102,14 +102,135 @@ Room.prototype.generateRoom = function() {
             }
         }
     }
-    else {
-        // Randomly place hazards
+    else if ( !this.isStart ) {
+        var safePath, startPoint, nextRoomIndex = 1, nextTarget;
+        
+        // Randomly place hazards, but not in the starting room
+        /*
         hazardCount = Util.randomInt(18, 3);
         for ( var c = 0; c < hazardCount; c++ ) {
             // Don't generate hazards at the walls >.>
             hX = Util.randomInt(7, 1);
             hY = Util.randomInt(7, 1);
             this.tiles[hX][hY] = 8;
+        }
+        */
+        var pickDoorTargetTile = function( direction ) {
+            var roomMiddle = Math.floor(this.roomsize / 2) - 1;
+            roomMiddle += Util.randomInt(1);
+            switch ( direction ) {
+                case 'N':
+                    x = roomMiddle;
+                    y = 1;
+                    break;
+                case 'S':
+                    x = roomMiddle;
+                    y = this.roomsize - 2;
+                    break;
+                case 'E':
+                    x = this.roomsize - 2;
+                    y = roomMiddle;
+                    break;
+                case 'W':
+                    x = 1;
+                    y = roomMiddle;
+                    break;
+            }
+            
+            return [x, y];
+        };
+        var generatePathTo = function( givenPath, target ) {
+            var nonviable = new Path();
+            var resets = 0;
+            var lastPoint = givenPath.getLast();
+            var curX = lastPoint[0], curY = lastPoint[1];
+            var direction, validDirs, nextd;
+            do {
+                // Compile a list of valid directions we can go
+                validDirs = [];
+                if ( !this.isEdgeTile(curX, curY - 1) && !givenPath.contains(curX, curY - 1) && direction !== 'S' && !nonviable.contains(curX, curY - 1) ) {
+                    validDirs.push('N');
+                }
+                if ( !this.isEdgeTile(curX, curY + 1) && !givenPath.contains(curX, curY + 1) && direction !== 'N' && !nonviable.contains(curX, curY + 1) ) {
+                    validDirs.push('S');
+                }
+                if ( !this.isEdgeTile(curX + 1, curY) && !givenPath.contains(curX + 1, curY) && direction !== 'W' && !nonviable.contains(curX + 1, curY) ) {
+                    validDirs.push('E');
+                }
+                if ( !this.isEdgeTile(curX - 1, curY) && !givenPath.contains(curX - 1, curY) && direction !== 'E' && !nonviable.contains(curX - 1, curY) ) {
+                    validDirs.push('W');
+                }
+                
+                if ( validDirs.length === 0 ) {
+                    // Dead end! Pop from the stack, and try again
+                    resets++;
+                    lastPoint = givenPath.pop();
+                    nonviable.push(lastPoint[0], lastPoint[1]);
+                    lastPoint = givenPath.getLast();
+                    curX = lastPoint[0];
+                    curY = lastPoint[1];
+                    if ( resets > 30 ) {
+                        console.log('something is broken again ... roomId: ' + this.roomId);
+                        console.log('path', givenPath);
+                        console.log('nonviable', nonviable);
+                        break;
+                    }
+                    continue;
+                }
+                nextd = validDirs[Util.randomInt(validDirs.length - 1)];
+                
+                switch ( nextd ) {
+                    case 'N':
+                        curY--;
+                        break;
+                    case 'S':
+                        curY++;
+                        break;
+                    case 'E':
+                        curX++;
+                        break;
+                    case 'W':
+                        curX--;
+                        break;
+                }
+                givenPath.push(curX, curY);
+                direction = nextd;
+            } while ( !givenPath.contains(target[0], target[1]) );
+            
+            return givenPath;
+        };
+        
+        // First, see if we need a path through the room
+        //  Dead-end rooms will only have one connecting room (and no exit)
+        // Doors are two tiles wide, so we'll randomly pick one
+        startPoint = pickDoorTargetTile.call(this, this.determineDirectionOf(this.connectedRooms[0]));
+        safePath = new Path(startPoint[0], startPoint[1]);
+        
+        if ( this.connectedRooms.length > 1 ) {
+            // Pathfind to other connected rooms
+            while ( this.connectedRooms[nextRoomIndex] ) {
+                nextTarget = pickDoorTargetTile.call(this, this.determineDirectionOf(this.connectedRooms[nextRoomIndex]));
+                if ( !safePath.contains(nextTarget[0], nextTarget[1]) ) {
+                    // Generate path
+                    safePath = generatePathTo.call(this, safePath, nextTarget);
+                }
+                
+                nextRoomIndex++;
+            }
+        }
+        if ( this.hasExit ) {
+            // Pathfind to the exit
+            nextTarget = pickDoorTargetTile.call(this, 'E');
+            if ( !safePath.contains(nextTarget[0], nextTarget[1]) ) {
+                safePath = generatePathTo.call(this, safePath, nextTarget);
+            }
+        }
+        
+        // TESTING ONLY: mark the path with the hazard tile for now
+        var temp;
+        for ( var i = 0, ln = safePath.points.length; i < ln; i++ ) {
+            temp = safePath.points[i];
+            this.tiles[temp[0]][temp[1]] = 8;
         }
     }
 };
